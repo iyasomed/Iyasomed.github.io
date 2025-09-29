c9b7be35a46b4bf158d60211a37edd602cba74b0
iyasomed <iyasomed@gmail.com>
2025-09-29 17:03:19 +0800

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-
-const backToTop = document.getElementById('backToTop');
-window.addEventListener('scroll', () => {
-    if (backToTop) {
-        backToTop.style.display = window.scrollY > 300 ? 'block' : 'none';
-    }
-});
